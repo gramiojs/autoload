fdbd914f5e64c33091c27ca65dda053a0a46f1e0
Kravets <57632712+kravetsone@users.noreply.github.com>
2024-05-12 17:36:52 +0300

    chore: remove process usage...

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,9 @@
+// @ts-nocheck
 import { isAbsolute, join } from "node:path";
 
 export function getPath(dir: string) {
+	if (typeof process === "undefined") return dir;
+
 	if (isAbsolute(dir)) return dir;
 	if (isAbsolute(process.argv[1])) return join(process.argv[1], "..", dir);
 
