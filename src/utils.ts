2d56f5f0c97659507db57c0dc2e098cc3e0b138c
Kravets <57632712+kravetsone@users.noreply.github.com>
2024-05-12 17:43:20 +0300

    chore: ignore process (Deno moment)

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,13 @@
-// @ts-nocheck
 import { isAbsolute, join } from "node:path";
 
 export function getPath(dir: string) {
+	// @ts-ignore Deno moment
 	if (typeof process === "undefined") return dir;
 
 	if (isAbsolute(dir)) return dir;
+	// @ts-ignore Deno moment
 	if (isAbsolute(process.argv[1])) return join(process.argv[1], "..", dir);
 
+	// @ts-ignore Deno moment
 	return join(process.cwd(), process.argv[1], "..", dir);
 }
