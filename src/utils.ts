9686e303a3cae64209baaf2e50c3b7dcffd7a90e
Kravets <57632712+kravetsone@users.noreply.github.com>
2024-05-13 12:46:36 +0300

    chore: import node:process to fix issue on Deno

--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,9 @@
 import { isAbsolute, join } from "node:path";
+import process from "node:process";
 
 export function getPath(dir: string) {
-	// @ts-ignore Deno moment
-	if (typeof process === "undefined") return dir;
-
 	if (isAbsolute(dir)) return dir;
-	// @ts-ignore Deno moment
 	if (isAbsolute(process.argv[1])) return join(process.argv[1], "..", dir);
 
-	// @ts-ignore Deno moment
 	return join(process.cwd(), process.argv[1], "..", dir);
 }
